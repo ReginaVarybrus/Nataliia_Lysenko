ad851d5b826b5f32ec85813df0051f01f0380dcf
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2025-02-08 18:34:20 +0100

    Updates

---
+++ assets/ArttherapyPage-CGvyYbhE.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CDoiXs_v.js";const s=()=>r.jsx("div",{children:r.jsx("h1",{children:"Arttherapy"})});export{s as default};
---
+++ assets/PricePage-DE2W-H38.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CDoiXs_v.js";const t=()=>e.jsx("div",{children:"PriceRoute"});export{t as default};
