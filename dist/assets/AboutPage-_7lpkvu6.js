b323c6121e5e9a8666781051bb7ec4416f70b208
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2025-02-08 18:36:15 +0100

    Deploy update

---
+++ dist/assets/AboutPage-_7lpkvu6.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CDoiXs_v.js";const e=()=>t.jsx("div",{children:"AboutRoute"});export{e as default};
---
+++ dist/assets/EventsPage-Ed_RjyyM.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CDoiXs_v.js";const s=()=>t.jsx("div",{children:"EventsRoute"});export{s as default};
