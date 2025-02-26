741c0ddb91d4f1e266e2f841b2b7a2bac515e2bd
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2025-02-26 07:34:01 +0100

    github deploy

--- dist/assets/AboutPage-Dap17WVc.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DL5gtVNr.js";const e=()=>t.jsx("div",{children:"AboutRoute"});export{e as default};
--- dist/assets/EventsPage-DvyuN-UL.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DL5gtVNr.js";const s=()=>t.jsx("div",{children:"EventsRoute"});export{s as default};
